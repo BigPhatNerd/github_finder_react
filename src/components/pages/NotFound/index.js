4a36dd71505080d3e3b9a817c44fbd0dc733bcae
Wilson <wilsonhorrell@git.training>
2020-09-23 14:57:29 -0500

    Added repos to search

--- src/components/pages/NotFound/index.js
+++ src/components/pages/NotFound/index.js
@@ -3,7 +3,7 @@ import React from 'react';
 const NotFound = () => {
     return (
         <div>
-        <h1> 🥺 Not found 🥺</h1>
+        <h1> <span role="img" aria-label="sad-face">🥺 Not found 🥺</span></h1>
         <p className="lead"> The page you are looking for does not exist...</p>
 		</div>
     )
